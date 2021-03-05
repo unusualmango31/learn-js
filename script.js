66fa8af20acd6d1488e9dbfd98ee012bb4562775
Anton <unusualmango3131@gmail.com>
2021-03-05 20:55:47 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// let numberOfFilms = +prompt('Сколько фильмов вы посмотрели?');
+
+// let personalMovieDB = {
+//     count : numberOfFilms,
+//     movies : {},
+//     actors : {},
+//     genres : [],
+//     privat : true
+// };
+
+// let a = prompt('Какой последний фильм вы посмотрели?');
+// let b = prompt('На сколько вы оцените его?');
+
+// personalMovieDB.movies[a] = b;
+
+// console.log(personalMovieDB);
+
