031e7e49ac9903826a14fe4d75121391bc4af99e
Anton <unusualmango3131@gmail.com>
2021-03-06 21:24:11 +0300

    practice №2

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-let numberOfFilms = Number(prompt("сколько фильмов вы посмотрели?")),
-    personalMovieDB = {
+let numberOfFilms = -1;
+
+while(isNaN(numberOfFilms) || numberOfFilms == "" || numberOfFilms <= 0 || numberOfFilms === null) {
+    numberOfFilms = Number(prompt("Сколько фильмов вы посмотрели?"));
+}
+
+let personalMovieDB = {
         count: numberOfFilms,
         movies: {},
         actors: {},
         genres: [],
         private: false,
-    };
+};
 
-let lastViewedFilm1 = prompt("Какой последний фильм вы посмотрели?"),
-    howRate1 = prompt("На сколько вы его оценили?"),
-    lastViewedFilm2 = prompt("Какой последний фильм вы посмотрели?"),
-    howRate2 = prompt("На сколько вы его оценили?");
+for(let i = 0; i < 2; i++) {
+    let lastViewedFilm = prompt("Какой последний фильм вы посмотрели?"),
+        howRate = Number(prompt("На сколько вы его оценили?"));
+    
+    if(lastViewedFilm != "" && lastViewedFilm != null && lastViewedFilm.length < 50 && howRate != null && !isNaN(howRate)) {
+        personalMovieDB.movies[lastViewedFilm] = howRate;
+        console.log("Значение принято");
+    } else {
+        console.log("Вы ввели некорректное значение");
+        i--;
+    }
+}
 
-personalMovieDB.movies[lastViewedFilm1] = howRate1;
-personalMovieDB.movies[lastViewedFilm2] = howRate2;
+if(personalMovieDB.count < 10) {
+    console.log("Просмотренно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический кинозритель");
+} else {
+    console.log("Вы настоящий киноман");
+}
 
 console.log(personalMovieDB);
-
